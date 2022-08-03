5b3a8a4ba926c7d6a39a72fc822b10eece6f0117
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-08-03 10:40:00 +0700

    add

---
+++ test_push_0308/test_push_0308/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_push_0308.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //test lan 1
+    }
+}
